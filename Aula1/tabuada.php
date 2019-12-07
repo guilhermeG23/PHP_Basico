48eacd3ce3fd16c41605cb30a0c126d88dd8d6c2
guilhermeG23 <guilhermebrechot@gmail.com>
2019-12-06 21:48:28 -0300

    Arrumando estrutura

---
+++ Aula1/tabuada.php
@@ -0,0 +1,6 @@
+<?php
+
+$valorT = 2;
+for ($valor=1;$valor<=10;$valor++) {
+	echo "$valorT X $valor = " . $valorT * $valor . PHP_EOL;
+}
---
+++ Aula1/variavel.php
@@ -0,0 +1,9 @@
+<?php
+
+const valor_estatico = "valor que nao pode ser alterado por ser contante";
+
+$variavel = "valor que pode ser variavel e pode ser modificado dependente a necessidade";
+
+echo valor_estatico;
+echo "<br>";
+echo $variavel;
